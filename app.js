5f7e57302f0d1e092864f0f142fc6ff37fd4a4db
Satya Yelamanchili <syelamanchili@ds.ihealthcs.com>
2017-06-23 00:14:46 -0500

    MeanStack application

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const bodyparcer = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config    =require('./config/database');
+const app = express();
+const users = require('./routes/users');
+const port = 4300;
+
+mongoose.connect(config.database);
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to database ' + config.database);
+});
+
+//Cors Middleware
+app.use(cors());
+
+//Body Parser Middleware 
+app.use(bodyparcer.json());
+app.use('/users', users);
+
+//Set static Folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint.');
+});
+
+
+//Start Server
+app.listen(port, ()=> {
+    console.log('Server started on port:' + port);
+});
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'localhost/meanauth',
+    secret: 'Satya'
+}
